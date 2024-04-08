37efa5e20a6fb480c4af49f4a02bf82e331072f5
sioel0 <sioel0@pm.me>
2024-04-08 22:13:21 +0200

    Updated qr code and fixed display problem

--- keyboards/splitkb/aurora/sweep/rev1/keymaps/sl0/keymap.c
+++ keyboards/splitkb/aurora/sweep/rev1/keymaps/sl0/keymap.c
@@ -111,6 +111,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 
 void render_sioel0_logo(void) {
 	oled_write_raw_P(sioel0_logo, sizeof(sioel0_logo));
+	oled_set_cursor(0, 8);
 }
 
 void render_github_qr(void) {
@@ -127,14 +128,14 @@ bool oled_task_user(void) {
 		
 		// activate line based on layer height 40
 		if(default_layer_state == _COL) {
-			oled_write_P(PSTR("_COL"), current_layer == _COL);
+			oled_write_P(PSTR(" COL "), current_layer == _COL);
 		} else if(default_layer_state == _QWE) {
-			oled_write_P(PSTR("_QWE"), current_layer == _QWE);
+			oled_write_P(PSTR(" QWE "), current_layer == _QWE);
 		}
-		oled_write_P(PSTR("_NAV"), current_layer == _NAV);
-		oled_write_P(PSTR("_SYM"), current_layer == _SYM);
-		oled_write_P(PSTR("_NUM"), current_layer == _NUM);
-		oled_write_P(PSTR("_HYP"), current_layer == _HYP);
+		oled_write_P(PSTR(" NAV "), current_layer == _NAV);
+		oled_write_P(PSTR(" SYM "), current_layer == _SYM);
+		oled_write_P(PSTR(" NUM "), current_layer == _NUM);
+		oled_write_P(PSTR(" HYP "), current_layer == _HYP);
 
 		// mod keys status height 8 bit
 		if(oneshot_mods & MOD_MASK_GUI || mods & MOD_MASK_GUI) {
@@ -142,7 +143,7 @@ bool oled_task_user(void) {
 		} else {
 			oled_write_P(PSTR("G"), false);
 		}
-		if(oneshot_mods & MOD_MASK_ALT || mods & MOD_MASK_ALT) {
+		if(oneshot_mods & MOD_BIT(KC_LALT) || mods & MOD_BIT(KC_LALT)) {
 			oled_write_P(PSTR("A"), true);
 		} else {
 			oled_write_P(PSTR("A"), false);
@@ -157,15 +158,21 @@ bool oled_task_user(void) {
 		} else {
 			oled_write_P(PSTR("S"), false);
 		}
+		if(oneshot_mods & MOD_BIT(KC_RALT) || mods & MOD_BIT(KC_RALT)) {
+			oled_write_P(PSTR("R"), true);
+		} else {
+			oled_write_P(PSTR("R"), false);
+		}
 		
 		// padding height 8 bit
-		oled_write_P(PSTR("    "), false);
+		oled_write_P(PSTR("      "), false);
 		
 		// check caps woc rd state height 8 bit
 		oled_write_P(is_caps_word_on() ? PSTR("CAPS") : PSTR("    "), false);
 	} else {
 		// render sioel0 logo height 64 bit
 		render_sioel0_logo();
+		render_github_qr();
 	}
 	return false;
 }
